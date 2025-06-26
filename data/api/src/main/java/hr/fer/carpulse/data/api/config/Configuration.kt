77d76fa4d35d076324139222fe028f0451159c10
rmajer10 <renato.majer@porsche-ep.com>
2025-06-26 20:45:49 +0200

    fix ktor issue

--- data/api/src/main/java/hr/fer/carpulse/data/api/config/Configuration.kt
+++ data/api/src/main/java/hr/fer/carpulse/data/api/config/Configuration.kt
@@ -9,7 +9,6 @@ import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.features.logging.LogLevel
 import io.ktor.client.features.logging.Logger
 import io.ktor.client.features.logging.Logging
-import io.ktor.client.features.observer.ResponseObserver
 import io.ktor.client.request.header
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
@@ -38,12 +37,6 @@ val ktorHttpClient = HttpClient(Android) {
             }
         }
         level = LogLevel.ALL
-    }
-
-    install(ResponseObserver) {
-        onResponse { response ->
-            Log.d("HTTP status:", "${response.status.value}")
-        }
     }
 
     install(DefaultRequest) {
